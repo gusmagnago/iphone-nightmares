e2a1dd42166baf285533af4160e155a55baff358
Gustavo Magnago <ext.gustavo.magnago@farfetch.com>
2023-10-31 15:24:35 +0200

    fix: Add Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { Analytics } from '@vercel/analytics/react';
+
 
 import {  OrbitControls } from '@react-three/drei';
 
@@ -10,24 +12,27 @@ import { palette } from './materials';
 
 export const App = () => {
 	return (
-		<Suspense fallback={null}>
-			<Canvas
-				camera={{
-					far: 10000,
-					position: new THREE.Vector3(0, -10, 500),
-				}}
-				dpr={window.devicePixelRatio}
-				shadows
-				flat
-				linear
-			>
-				<color attach='background' args={['#eaeaea']} />
-				<hemisphereLight intensity={2} color={palette.glass} />
-				<directionalLight color={palette.light} intensity={1.5} position={[1, 1, 1]}/>
-				<Scene />
-				<OrbitControls />
-			</Canvas>
-		</Suspense>
+		<>
+			<Suspense fallback={null}>
+				<Canvas
+					camera={{
+						far: 10000,
+						position: new THREE.Vector3(0, -10, 500),
+					}}
+					dpr={window.devicePixelRatio}
+					shadows
+					flat
+					linear
+				>
+					<color attach='background' args={['#eaeaea']} />
+					<hemisphereLight intensity={2} color={palette.glass} />
+					<directionalLight color={palette.light} intensity={1.5} position={[1, 1, 1]}/>
+					<Scene />
+					<OrbitControls />
+				</Canvas>
+			</Suspense>
+			<Analytics />
+		</>
 	);
 };
 
